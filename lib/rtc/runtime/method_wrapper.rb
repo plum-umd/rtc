d953f9d31656ee1ac02dd0ae6dba34f0c1771887
John Toman <jtoman>
2012-02-19 20:39:05 -0500

    Adds in a new way of wrapping methods that does not require magic properties set on the object, a bunch of eval, and it lets the interpreter bind arguments for us. The seems to provide a cleaner design, and it let's the VM handle how to bind optional and rest arguments

---
+++ lib/rtc/runtime/method_wrapper.rb
@@ -0,0 +1,75 @@
+module Rtc
+  class MethodWrapper
+    class NoArgument; end
+    def self.make_wrapper(class_obj, method_name)
+      MethodWrapper.new(class_obj, method_name)
+    end
+    def invoke(invokee, arg_vector)
+      puts "TODO: put argument checking here!"
+      regular_args = arg_vector[:args]
+      passed_and_formals = regular_args.zip(@no_block_params)
+      arg_list = []
+      passed_and_formals.each {
+        # maybe argument checking would be better here?
+        |p_f|
+        passed,formal = p_f
+        if formal[0] == :opt and passed.instance_of?(Rtc::MethodWrapper::NoArgument)
+          return
+        elsif formal[0] == :rest
+          arg_list += passed
+        else
+          arg_list << passed
+        end
+      }
+      if arg_vector[:block]
+        ret_value = @original_method.bind(invokee).call(*arg_list, &arg_vector[:block])
+      else
+        ret_value = @original_method.bind(invokee).call(*arg_list)
+      end
+      puts "TODO: puts return value checking here!"
+      ret_value
+    end
+    private
+    def initialize(class_obj,method_name)
+      @method_name = method_name
+      @class_obj = class_obj
+      this_obj = self
+      @original_method = class_obj.instance_method(method_name)
+      @no_block_params = @original_method.parameters.reject { |param_spec| param_spec[0] == :block }
+      wrapper_lambda = eval("lambda {
+        |#{gen_arg_string()}|
+        args = #{gen_collapse_args()}
+        this_obj.invoke(self, args)
+      }")
+      class_obj.send(:define_method, method_name, wrapper_lambda)
+    end
+    
+    def make_single_arg_string(parameter)
+      case parameter[0]
+      when :req
+        parameter[1].to_s
+      when :opt
+        "#{parameter[1].to_s} = Rtc::MethodWrapper::NoArgument.new"
+      when :rest
+        "*#{parameter[1].to_s}"
+      when :block
+        # TODO(jtoman): put error reporting here, I don't really what the method for ruby is...
+        # this can't happen
+      else
+        # harumph
+      end
+    end
+    def gen_arg_string()
+      (@no_block_params.map {
+        |param_spec|
+        make_single_arg_string(param_spec)
+      } + ["&block"]).join(", ")
+    end
+    def gen_collapse_args()
+      "{ :args => [" + @no_block_params.map {
+        |param_spec|
+        param_spec[1].to_s
+      }.join(", ") + "], :block => block}"
+    end
+  end
+end
