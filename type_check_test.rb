9d2cd5fed2c5a997f6af58009313e34f5dda16c9
John Toman <jtoman>
2012-02-24 20:42:02 -0500

    Adds some type check tests for the new symbols

--- type_check_test.rb
+++ type_check_test.rb
@@ -9,6 +9,15 @@ class MyClass
   def f(*a)
     a[0] + a[-1]
   end
+
+  typesig("my_method: (:foo or :bar) -> Fixnum")
+  def my_method(b)
+    5
+  end
+  typesig("my_other_method: (Array<:subscribers or :talks or :subscribed_talks>) -> Fixnum")
+  def my_other_method(b)
+    6
+  end
 end
 
 test_instance = MyClass.new
@@ -16,3 +25,5 @@ f_sig = test_instance.rtc_typeof("f")
 
 MyClass.new.f(1,2)
 puts "success!"
+MyClass.new.my_method(:foo)
+MyClass.new.my_other_method([:subscribers, :talks])
