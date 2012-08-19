fc6ad33545e1d5275d7f5027c6195ab138e3a30f
j2ren <janeren3@gmail.com>
2012-08-19 13:27:04 -0400

    various changes

--- lib/rtc/runtime/class_modifier.rb
+++ lib/rtc/runtime/class_modifier.rb
@@ -4,18 +4,31 @@ require 'rtc/typing/types'
 
 module Rtc 
   class ClassModifier
+    attr_reader :cache 
+
     @@cache = {}
+    @@class_parameters = {}
+
     def self.handle_class_annot(annot_obj)
       begin
         the_obj = eval(annot_obj.qualified_name)
         Rtc::Types::NominalType.of(the_obj).type_parameters = annot_obj.parameters
+
+        n = Rtc::Types::NominalType.of(the_obj)
+        t = Rtc::Types::ParameterizedType.new(n, annot_obj.parameters)
+        @@class_parameters[the_obj] = t
+
         the_obj
       rescue => e
         @@cache[annot_obj.qualified_name] = annot_obj.parameters
         nil
       end
     end
-    
+
+    def self.get_class_parameters
+      @@class_parameters
+    end
+
     def self.modify_class(class_obj)
       if @@cache[class_obj.name]
         Rtc::Types::NominalType.of(class_obj).type_parameters = @@cache[class_obj.name]
