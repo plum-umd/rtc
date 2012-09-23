1298d0620f52d77ddd66e4f05e14aa099d32808d
Stevie Strickland <sstrickl@racket-lang.org>
2012-09-22 23:13:26 -0400

    Add annotations to SudokuSolver instances, update profile.
    The last profile I had was ~2550 after the first profile-guided change
    I made (I didn't commit it).  Now we're at 611, so that's good!  Still
    really heavy as overhead, but at least we've removed 75% of the overhead.

--- examples/sudokusolver/test/driver.rb
+++ examples/sudokusolver/test/driver.rb
@@ -6,5 +6,5 @@
 require 'sudokusolver'
 
 puzzle = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
-s  = SudokuSolver.new
+s  = SudokuSolver.new.rtc_annotate("SudokuSolver")
 s.print_grid(s.search(s.parse_grid(puzzle)))
