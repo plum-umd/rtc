33d622188481728de750d180b4fd7e214b15a22e
Stevie Strickland <sstrickl@cs.umd.edu>
2013-02-26 17:34:32 -0500

    More work on routing DSL specs, including things to fix (commented out)

--- dsl/specs/routing.rb
+++ dsl/specs/routing.rb
@@ -1,3 +1,16 @@
+module RoutingHelper
+  def self.get_class(name)
+    Module.const_get name
+  end
+
+  def self.class_exists?(name)
+    c = get_class name
+    c.is_a? Class
+  rescue NameError
+    false
+  end
+end
+
 class ActionDispatch::Routing::RouteSet
   extend Dsl
 
@@ -25,9 +38,46 @@ class ActionDispatch::Routing::RouteSet
   resources_spec = Dsl.create_spec do
     dsl do
       spec :resources do
+        pre_cond do |*args|
+          args.all? do |a|
+            if a.is_a? String or a.is_a? Symbol
+            then 
+              cname = "#{a.capitalize}Controller"
+              p "Checking for class #{cname}"
+              RoutingHelper.class_exists? cname
+            else true
+            end
+          end
+        end
         include_spec resources_spec
         include_spec get_post_spec
         include_spec logging_spec, "resources"
+        # post_cond do |ret, *args|
+        #   args.all? do |a|
+        #     if a.is_a? String or a.is_a? Symbol
+        #     then 
+        #       # how to get the Application?
+        #       app.method_defined? "#{a.downcase}_path"
+        #     else true
+        #     end
+        #   end
+        # end
+      end
+      spec :resource do
+        # pre_cond do |*args|
+        #   args.all? do |a|
+        #     if a.is_a? String or a.is_a? Symbol
+        #     then 
+        #       cname = "#{a.capitalize}sController"
+        #       p "Checking for class #{cname}"
+        #       RoutingHelper.class_exists? cname
+        #     else true
+        #     end
+        #   end
+        # end
+        include_spec resources_spec
+        include_spec logging_spec, "resource"
+        include_spec get_post_spec
       end
       spec :collection do
         include_spec logging_spec, "collection"
