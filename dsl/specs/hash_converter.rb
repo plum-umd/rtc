0bbce3a349cec02e6e28a928c34bca4205b67193
Stevie Strickland <sstrickl@cs.umd.edu>
2013-03-19 13:29:02 -0400

    Fix up the HashConverter spec.
    Move the specs for functions available in the block argument to
    HashConverter.convert within a dsl block in that method's specs.
    
    Avoid using global state as much as possible, and route any actual
    global state through the Dsl.state hash.
    
    The blocks for pre/post_conds and the like don't (or shouldn't) get
    the block given to the method, so remove arguments that make it look
    like they do.

--- dsl/specs/hash_converter.rb
+++ dsl/specs/hash_converter.rb
@@ -2,48 +2,46 @@
 
 require "hash_converter"
 
-class HashConverter
+class << HashConverter
   extend Dsl
 
-  class << self
-    extend Dsl
-
-    spec :convert do
-      pre_task do |arg, blk|
-        $h = arg
-      end
+  spec :convert do
+    pre_task do |arg|
+      @Dsl.state[:hashconv] = arg
     end
 
-    spec :path do
-      pre_task do |arg, blk|
-        $k = [] if not $k
-        $k.push(arg.to_sym)
+    dsl do
+      spec :path do
+        pre_task do |arg|
+          @spec_path = [] if not @spec_path
+          @spec_path.push(arg.to_sym)
+        end
+
+        pre_cond do |arg|
+          x = @Dsl.state[:hashconv]
+
+          @spec_path.all? { |i|
+            x.keys.include?(i)
+            x = x[i]
+          }
+        end
+
+        post_task do
+          @spec_path.pop
+        end
       end
 
-      pre_cond do |arg, blk|
-        x = $h
+      spec :map do
+        pre_cond do |arg1, arg2, arg3|
+          x = @Dsl.state[:hashconv]
+          @spec_path.each {|i| x = x[i]}
 
-        $k.all? {|i|
-          x.keys.include?(i)
-          x = x[i]
-        }
-      end
-
-      post_task do 
-        $k.pop
-      end
-    end
-
-    spec :map do
-      pre_cond do |arg1, arg2, arg3, blk|
-        x = $h
-        $k.each {|i| x = x[i]}
-
-        arg1.gsub(KEY_REGEX).all? {|v| 
-          v = v.split('{')[1]
-          v = v.split('}')[0].to_sym
-          x.keys.include?(v)
-        }
+          arg1.gsub(KEY_REGEX).all? {|v|
+            v = v.split('{')[1]
+            v = v.split('}')[0].to_sym
+            x.keys.include?(v)
+          }
+        end
       end
     end
   end
