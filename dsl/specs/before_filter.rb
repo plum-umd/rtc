62acdd7dc4780d7061c629bbf4f4ca1424666281
spiderpig <spiderpig@spiderpig-VirtualBox.(none)>
2013-03-25 11:10:11 -0400

    now supports all filters, added check for conflicting arg to filter, and fixed bug for :except option

--- dsl/specs/before_filter.rb
+++ dsl/specs/before_filter.rb
@@ -9,13 +9,73 @@ end
 
 module AbstractController::Callbacks::ClassMethods
   extend Dsl
-  
-  spec :before_filter do
+
+  check_method_spec = Dsl.create_spec do 
     pre_task do |*filters|
-      Dsl.state[:before_filter_methods] = [] if not Dsl.state[:before_filter_methods]
-      
-      Dsl.state[:before_filter_methods].push([filters, self])
+      Dsl.state[:filter_methods] = [] if not Dsl.state[:before_filter_methods]
+      Dsl.state[:filter_methods].push([filters, self])
+    end    
+
+    pre_cond "method listed both as :except and :only" do |method_name, h|
+      only_opt = h[:only]
+      except_opt = h[:except]
+
+      if only_opt and except_opt 
+        only_opt.all? {|m|
+          not except_opt.include?(m)
+        }
+      else
+        true
+      end
     end
+  end
+
+  spec :before_filter do
+    include_spec check_method_spec
+  end
+
+  spec :prepend_before_filter do
+    include_spec check_method_spec
+  end
+
+  spec :skip_before_filter do
+    include_spec check_method_spec
+  end
+
+  spec :append_before_filter do
+    include_spec check_method_spec
+  end
+
+  spec :after_filter do
+    include_spec check_method_spec
+  end
+
+  spec :prepend_after_filter do
+    include_spec check_method_spec
+  end
+
+  spec :skip_after_filter do
+    include_spec check_method_spec
+  end
+
+  spec :append_after_filter do
+    include_spec check_method_spec
+  end
+
+  spec :around_filter do
+    include_spec check_method_spec
+  end
+
+  spec :prepend_around_filter do
+    include_spec check_method_spec
+  end
+
+  spec :skip_around_filter do
+    include_spec check_method_spec
+  end
+
+  spec :append_around_filter do
+    include_spec check_method_spec
   end
 end
 
@@ -23,10 +83,10 @@ class ActionDispatch::Routing::RouteSet::Dispatcher
   extend Dsl
   
   spec :controller_reference do
-    post_cond do |controller|
+    post_cond "filter has undefined method" do |controller|
       methods = []
       
-      bfm = Dsl.state[:before_filter_methods]
+      bfm = Dsl.state[:filter_methods]
       bfm and bfm.each {|e| methods.push(e) if e[1] == controller }
       
       methods.all? { |e|
@@ -35,7 +95,18 @@ class ActionDispatch::Routing::RouteSet::Dispatcher
         method_found = BFHelper.has_instance_method?(controller, m)
         if options.size > 1
           only_methods = options[1][:only]
-          method_found and only_methods.all? {|n|
+          except_methods = options[1][:except]
+          only_except_methods = []
+
+          [only_methods, except_methods].each {|t|
+            if t
+              t.each {|i|
+                only_except_methods.push(i)
+              }
+            end
+          }
+
+          method_found and only_except_methods.all? {|n|
             BFHelper.has_instance_method?(controller, n)
           }
         else method_found
