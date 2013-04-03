14397815edf7657c63030c7a2733ca90b1b044bb
Justin Terry <jterry64@gmail.com>
2013-04-02 22:44:56 -0400

    Added a thing to walk through ObjectSpace and check if any classes loaded before rtc have type annotations

--- lib/rtc_lib.rb
+++ lib/rtc_lib.rb
@@ -6,6 +6,18 @@ Rtc::MasterSwitch.turn_on
 
 # constant set of all noncore classes currently annotated
 NONCORE_ANNOTATED_CLASSES = Set.new ["csv"]
+NONCORE_ANNOTATIONS_BASE_PATH = File.expand_path File.dirname(__FILE__) + \
+      "/rtc/typing/noncore_base_types/base_"
+
+loaded_classes = Set.new(ObjectSpace.each_object(Class)).collect! { 
+  |c| 
+  c.to_s.downcase 
+} 
+
+NONCORE_ANNOTATED_CLASSES.intersection(loaded_classes).each {
+  |c|
+  load(NONCORE_ANNOTATIONS_BASE_PATH + c + ".rb")
+}
 
 # intercept require and load class's type annotations, if they exist
 alias :old_require :require
@@ -13,7 +25,6 @@ def require(name)
   old_require name
 
   if NONCORE_ANNOTATED_CLASSES.member?(name)
-    load(File.expand_path File.dirname(__FILE__) + \
-      "/rtc/typing/noncore_base_types/base_" + name + ".rb") 
+    load(NONCORE_ANNOTATIONS_BASE_PATH + name + ".rb")
   end
 end
