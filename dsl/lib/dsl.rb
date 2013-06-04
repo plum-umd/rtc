b3d96e24afa86e8835c1938fc6f531b73228727b
Stevie Strickland <sstrickl@cs.umd.edu>
2013-06-04 15:00:52 -0400

    Refactoring splitting DslSpec and MethodProxy.

--- dsl/lib/dsl.rb
+++ dsl/lib/dsl.rb
@@ -1,58 +1,56 @@
-class MethodProxy
-  @@gensym = 0
+module Dsl
+  class MethodProxy
+    def self.gensym
+      if @gensym
+        gsym = @gensym
+      else
+        gsym = 0
+      end
 
-  def initialize(kls, mname)
-    @pres = []
-    @posts = []
-    @action = nil
-    @class = kls
-    @mname = mname
-  end
+      @gensym = gsym + 1
+    end
 
-  def action(&blk)
-    @action = blk
-  end
+    def self.pre_block(kls, mname, &blk)
+      old_mname = "__rtc_old_#{mname}#{gensym}"
 
-  def register_pre(&blk)
-    @pres.push(blk)
-  end
+      kls.class_eval do
+        alias_method old_mname, mname
 
-  def register_post(&blk)
-    @posts.unshift(blk)
-  end
-
-  def apply
-    gensym = @@gensym
-    @@gensym = gensym + 1
-    mname = @mname
-    old_mname = "__rtc_old_#{mname}#{gensym}"
-    action = @action
-    pres = @pres
-    posts = @posts
-
-    @class.class_eval do
-      alias_method old_mname, mname unless action
-
-      define_method(mname) do |*args, &blk|
-        new_args = pres.reduce(args + [blk]) do |args, b|
-          instance_exec(*args, &b)
+        define_method mname do |*args, &b|
+          *new_args, new_b = instance_exec(*args, b, &blk)
+          send old_mname, *new_args, &new_b
         end
-        *new_args, blk = new_args if blk
-        if action
-          r = action.call(*new_args, &blk)
-        else
-          r = send old_method, *new_args, &blk
+      end
+    end
+
+    def self.post_block(kls, mname, &blk)
+      old_mname = "__rtc_old_#{mname}#{gensym}"
+
+      kls.class_eval do
+        alias_method old_mname, mname
+
+        define_method mname do |*args, &b|
+          r = send old_mname, *args, &b
+          instance_exec(r, *args, b, &blk)
         end
-        new_r, _ = posts.reduce([r] + new_args + [blk]) do |args, b|
-          instance_exec(*args, &b)
-        end
-        new_r
       end
     end
   end
-end
 
-module Dsl
+  class DslSpec
+    def initialize(kls, mname)
+      @class = kls
+      @mname = mname
+    end
+
+    def action(&blk)
+      mname = @mname
+      @class.class_eval do
+        define_method mname, &blk
+      end
+    end
+  end
+
   @state = {}
 
   def self.state
@@ -64,78 +62,60 @@ module Dsl
   end
 
   def spec(method, &block)
-    if instance_variable_get(:@dsl_gensym)
-    then gensym = instance_variable_get(:@dsl_gensym) + 1
-    else gensym = 0
-    end
-    instance_variable_set(:@dsl_gensym, gensym)
-
-    proxy = MethodProxy.new(self, method)
-    proxy.instance_eval(&block)
-    proxy.apply
+    obj = DslSpec.new(self, method)
+    obj.instance_eval(&block)
   end
 end
 
-class MethodProxy
-  extend Dsl
-
+class Dsl::DslSpec
   class PreConditionFailure < Exception; end
   class PostConditionFailure < Exception; end
 
-  spec :pre_cond do
-    action do |desc = "", block|
-      register_pre do |*args|
-        raise PreConditionFailure, desc unless instance_exec(*args, &block)
-        args
-      end
+  def pre_cond (desc = "", &blk)
+    Dsl::MethodProxy.pre_block(@class, @mname) do |*args|
+      raise PreConditionFailure, desc unless instance_exec(*args, &blk)
+      args
     end
   end
 
-  spec :post_cond do
-    action do |desc = "", block|
-      register_post do |*args|
-        raise PostConditionFailure, desc unless instance_exec(*args, &block)
-        args
-      end
+  def post_cond (desc = "", &blk)
+    Dsl::MethodProxy.post_block(@class, @mname) do |*args|
+      raise PostConditionFailure, desc unless instance_exec(*args, &blk)
+      args
     end
   end
 
-  spec :pre_task do
-    action do |block|
-      register_pre do |*args|
-        instance_exec(*args, &block)
-        args
-      end
+  def pre_task (&blk)
+    Dsl::MethodProxy.pre_block(@class, @mname) do |*args|
+      instance_exec(*args, &blk)
+      args
     end
   end
 
-  spec :post_task do
-    action do |block|
-      register_post do |*args|
-        instance_exec(*args, &block)
-        args
-      end
+  def post_task (&blk)
+    Dsl::MethodProxy.post_block(@class, @mname) do |*args|
+      instance_exec(*args, &blk)
+      args
     end
   end
-      
-  spec :dsl do
-    action do |b|
-      register_pre do |*a, blk|
-        new_blk = Proc.new do |*args|
+
+  def dsl (&blk)
+    Dsl::MethodProxy.pre_block(@class, @mname) do |*args, b|
+      if b
+        new_b = Proc.new do |*args|
           ec = singleton_class
           ec.extend Dsl
-          ec.class_exec(*args, &b)
-          self.instance_exec(*args, &blk)
+          ec.class_eval &blk
+          instance_exec(*args, &b)
         end
-        a + [new_blk]
+        args + [new_b]
+      else args
       end
     end
   end
 
-  spec :include_spec do
-    action do |blk, *args|
-      instance_exec(*args, blk)
-    end
+  def include_spec (blk, *args)
+    instance_exec(*args, &blk)
   end
 
 end
