d6c33c2d4b88421b2e8a275c2ed904fc63548f87
Stevie Strickland <sstrickl@cs.umd.edu>
2013-06-09 17:06:47 -0400

    Changing the Dsl basis to be more general than a basis for specs.
    Now the basis can describe other DSLs, not just specs of DSLs.  There
    are two entry points: entry, which describes and creates a new DSL entry
    method, and spec, which describes an existing method.  The DSL keywords
    that overwrite methods (action/dsl_rec/dsl) are only useable from entry.

--- dsl/lib/dsl.rb
+++ dsl/lib/dsl.rb
@@ -1,121 +1,160 @@
 module Dsl
-  class MethodProxy
-    def self.gensym
+  class Spec
+    def initialize(cls, mname)
+      @class = cls
+      @mname = mname
+
+      unless cls.method_defined? mname
+        raise "Method #{mname} not defined for #{cls}"
+      end
+    end
+
+    # Takes a block that transforms the incoming arguments
+    # into (possibly) new arguments to be fed to the method.
+    def pre(&b)
+      mname = @mname
+      old_mname = "__dsl_old_#{mname}_#{gensym}"
+
+      @class.class_eval do
+        alias_method old_mname, mname
+
+        define_method mname do |*args, &blk|
+          *new_args, new_blk = instance_exec(*args, blk, &b)
+          send old_mname, *new_args, &new_blk
+        end
+      end
+    end
+
+    # Takes a block that transforms the return value
+    # into a (possibly) return value to be returned from the method.
+    # The block also gets handed the original arguments.
+    def post(&b)
+      mname = @mname
+      old_mname = "__dsl_old_#{mname}_#{gensym}"
+
+      @class.class_eval do
+        alias_method old_mname, mname
+
+        define_method mname do |*args, &blk|
+          res = send old_mname, *args, &blk
+          instance_exec(res, *args, blk, &b)
+        end
+      end
+    end
+
+    # pre/post_task are versions of pre/post that ignore the
+    # return value from the block and just pass along the
+    # original arguments or return value.
+
+    def pre_task(&b)
+      pre do |*args|
+        instance_exec(*args, &b)
+        args
+      end
+    end
+
+    def post_task(&b)
+      post do |r, *args|
+        instance_exec(r, *args, &b)
+        r
+      end
+    end
+
+    class PreConditionFailure < Exception; end
+    class PostConditionFailure < Exception; end
+
+    # pre/post_cond are like pre/post_task, except they check
+    # the block return and error if the block returns false/nil.
+
+    def pre_cond(desc = "", &b)
+      pre_task do |*args|
+        raise PreConditionFailure, desc unless instance_exec(*args, &b)
+      end
+    end
+
+    def post_cond(desc = "", &b)
+      post_task do |r, *args|
+        raise PostConditionFailure, desc unless instance_exec(r, *args, &b)
+      end
+    end
+
+    private
+
+    def gensym
       if @gensym
         gsym = @gensym
       else
         gsym = 0
       end
-
       @gensym = gsym + 1
-    end
-
-    def self.pre_block(kls, mname, &blk)
-      old_mname = "__rtc_old_#{mname}#{gensym}"
-
-      kls.class_eval do
-        alias_method old_mname, mname
-
-        define_method mname do |*args, &b|
-          *new_args, new_b = instance_exec(*args, b, &blk)
-          send old_mname, *new_args, &new_b
-        end
-      end
-    end
-
-    def self.post_block(kls, mname, &blk)
-      old_mname = "__rtc_old_#{mname}#{gensym}"
-
-      kls.class_eval do
-        alias_method old_mname, mname
-
-        define_method mname do |*args, &b|
-          r = send old_mname, *args, &b
-          instance_exec(r, *args, b, &blk)
-        end
-      end
+      gsym
     end
   end
 
-  class DslSpec
-    def initialize(kls, mname)
-      @class = kls
+  class Keyword < Spec
+    def initialize(cls, mname)
+      if cls.method_defined? mname
+        raise "Method #{mname} already defined for #{cls}"
+      end
+
+      @class = cls
       @mname = mname
+
+      action { nil }
     end
 
+    # For non-DSL keywords
     def action(&blk)
       mname = @mname
+
       @class.class_eval do
         define_method mname, &blk
       end
     end
-  end
 
-  @state = {}
-
-  def self.state
-    @state
-  end
-
-  def self.create_spec(&blk)
-    Proc.new &blk
-  end
-
-  def spec(method, &block)
-    obj = DslSpec.new(self, method)
-    obj.instance_eval(&block)
-  end
-end
-
-class Dsl::DslSpec
-  class PreConditionFailure < Exception; end
-  class PostConditionFailure < Exception; end
-
-  def pre_cond (desc = "", &blk)
-    Dsl::MethodProxy.pre_block(@class, @mname) do |*args|
-      raise PreConditionFailure, desc unless instance_exec(*args, &blk)
-      args
+    # For keywords that take the same DSL they are in.
+    def dsl_rec
+      action do |*args, &blk|
+        instance_exec(*args, &blk)
+      end
     end
-  end
 
-  def post_cond (desc = "", &blk)
-    Dsl::MethodProxy.post_block(@class, @mname) do |*args|
-      raise PostConditionFailure, desc unless instance_exec(*args, &blk)
-      args
-    end
-  end
+    # For keywords that take a different DSL than they are in.
+    def dsl(cls = nil, *a, &b)
+      mname = @mname
 
-  def pre_task (&blk)
-    Dsl::MethodProxy.pre_block(@class, @mname) do |*args|
-      instance_exec(*args, &blk)
-      args
-    end
-  end
+      raise "Need a class or block" unless cls or b
 
-  def post_task (&blk)
-    Dsl::MethodProxy.post_block(@class, @mname) do |*args|
-      instance_exec(*args, &blk)
-      args
-    end
-  end
+      unless b.nil?
+        cls = Class.new(Object) if cls.nil?
+        Lang.new(cls).instance_exec(*a, &b)
+      end
 
-  def dsl (&blk)
-    Dsl::MethodProxy.pre_block(@class, @mname) do |*args, b|
-      if b
-        new_b = Proc.new do |*args|
-          ec = singleton_class
-          ec.extend Dsl
-          ec.class_eval &blk
-          instance_exec(*args, &b)
-        end
-        args + [new_b]
-      else args
+      action do |*args, &blk|
+        cls.new(*a).instance_exec(*args, &blk)
       end
     end
   end
 
-  def include_spec (blk, *args)
-    instance_exec(*args, &blk)
+  class Lang
+    def initialize(cls)
+      @class = cls
+    end
+
+    def keyword(mname, *args, &blk)
+      Keyword.new(@class, mname).instance_exec(*args, &blk)
+    end
+
+    def spec(mname, *args, &blk)
+      Spec.new(@class, mname).instance_exec(*args, &blk)
+    end
   end
 
+  def entry(mname,*args, &blk)
+    Lang.new(self).keyword(mname, &blk)
+  end
+
+  def spec(mname, *args, &blk)
+    Lang.new(self).spec(mname, &blk)
+  end
 end
