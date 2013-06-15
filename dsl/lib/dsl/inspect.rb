39e86c28ca71ed60d433ef01a2042ec85cf0458f
Stevie Strickland <sstrickl@cs.umd.edu>
2013-06-15 03:03:44 -0400

    Have to add |*args| here for reasons listed in previous commit.

--- dsl/lib/dsl/inspect.rb
+++ dsl/lib/dsl/inspect.rb
@@ -3,7 +3,7 @@ require 'dsl'
 class Dsl::Spec
   def inspect(*events, &blk)
     trace = TracePoint.new(*events, &blk)
-    pre_task { trace.enable }
-    post_task { trace.disable }
+    pre_task { |*args| trace.enable }
+    post_task { |*args| trace.disable }
   end
 end
