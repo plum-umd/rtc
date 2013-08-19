24c57a4b9b9b4ff99058e064eea5c1436613a98e
Stevie Strickland <sstrickl@cs.umd.edu>
2013-08-19 15:32:31 -0400

    This is now its own repo (plum-umd/rdl).

--- dsl/lib/dsl/inspect.rb
+++
@@ -1,9 +0,0 @@
-require 'dsl'
-
-class Dsl::Spec
-  def inspect(*events, &blk)
-    trace = TracePoint.new(*events, &blk)
-    pre_task { |*args| trace.enable }
-    post_task { |*args| trace.disable }
-  end
-end
